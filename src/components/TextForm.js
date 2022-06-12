4e31250bdb5aae889caa6707b3365921cfc84f5c
Jesus_Sheep_Akash <akashmaur94@gmail.com>
2022-06-12 15:09:07 +0300

    wordCountApp

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -25,17 +25,25 @@ export default function TextForm(props) {
 
     const [text, setText] = useState("Default Text");
     return (
+        <>
+            <div className="mb-3 my-3">
+                <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.exampleLable}</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
 
-        <div className="mb-3 my-3">
-            <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.exampleLable}</label>
-            <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
 
-            <div className="mx-2">
                 <button className="btn btn-primary my-3" onClick={handleUpClick}>Conver To UpperCase</button>
-                &nbsp;
-                <button className="btn btn-warning my-3" onClick={handleLoClick}>Conver To LowerCase</button>
-            </div>
-        </div>
 
+                <button className="btn btn-warning mx-3" onClick={handleLoClick}>Conver To LowerCase</button>
+
+            </div>
+            <div className="container">
+                <h1>Your TextSummery</h1>
+                <p>Your Words : {text.split(" ").length} and Characters: {text.length}</p>
+                <p>{0.008 * text.split(" ").length} Minutes Read</p>
+                <p>Preview : <br />
+                    {text}
+                </p>
+            </div>
+        </>
     )
 }
